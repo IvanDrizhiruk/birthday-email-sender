a3dcd0179c7bdec0253ca76e51d163dc4a2c6f91
ivan <ivan.drizhiruk@gmail.com>
2021-12-26 22:23:30 +0200

    Add skeleton implementation  for EmailSender

--- src/main/java/ua/dp/dryzhyruk/impl/email/sender/EmailSenderImpl.java
+++ src/main/java/ua/dp/dryzhyruk/impl/email/sender/EmailSenderImpl.java
@@ -1,15 +1,47 @@
 package ua.dp.dryzhyruk.impl.email.sender;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import ua.dp.dryzhyruk.email.generator.EmailContent;
 import ua.dp.dryzhyruk.email.sender.EmailSender;
 import ua.dp.dryzhyruk.recipient.group.EmailData;
 
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+@Slf4j
 @Service
 public class EmailSenderImpl implements EmailSender {
 
     @Override
-    public void sendEmail(EmailData recipientGroup, EmailContent generatedEmail) {
+    public void sendEmail(EmailData emailData, EmailContent emailContent) {
+        String to = emailData.getTo().getRecipientEmail();
+        String from = "no-reply@dxc.com";
+        String host = "localhost";
 
+        Properties properties = System.getProperties();
+        properties.setProperty("mail.smtp.host", host);
+        Session session = Session.getDefaultInstance(properties);
+
+        try {
+            MimeMessage mimeMessage = new MimeMessage(session);
+            MimeMessageHelper message = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+            message.setFrom(from);
+            message.setTo(to);
+            message.setSubject("This is the Mega Subject!!!"); //TODO
+            message.setText(emailContent.getHtmlContent(), true);
+//            @Value("classpath:/mail-logo.png")
+//            private Resource resourceFile;
+//            message.addInline("attachment.png", resourceFile);
+
+
+            Transport.send(mimeMessage);
+        } catch (MessagingException e) {
+            log.error("Error", e);
+        }
     }
 }
