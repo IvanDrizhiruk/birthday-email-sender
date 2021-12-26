95c7b43418dca29e3457c14921d298ccb821d4c1
ivan <ivan.drizhiruk@gmail.com>
2021-12-26 15:47:18 +0200

    Add mail sender and template processing

---
+++ src/test/java/ua/dp/dryzhyruk/email/BirthdayEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.dp.dryzhyruk.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthdayEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
