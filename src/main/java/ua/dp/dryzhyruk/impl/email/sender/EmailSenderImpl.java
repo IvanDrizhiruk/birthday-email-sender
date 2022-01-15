65528a8b1d125296b9c4e53d60f78aca2abea6db
ivan <ivan.drizhiruk@gmail.com>
2022-01-15 19:38:40 +0200

    Fix sending of mail

--- src/main/java/ua/dp/dryzhyruk/impl/email/sender/EmailSenderImpl.java
+++ src/main/java/ua/dp/dryzhyruk/impl/email/sender/EmailSenderImpl.java
@@ -8,9 +8,7 @@ import org.springframework.stereotype.Service;
 import ua.dp.dryzhyruk.ports.email.data.EmailData;
 import ua.dp.dryzhyruk.ports.email.sender.EmailSender;
 
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
+import javax.mail.*;
 import javax.mail.internet.MimeMessage;
 import java.io.File;
 import java.nio.charset.StandardCharsets;
@@ -20,13 +18,37 @@ import java.util.Properties;
 @Service
 public class EmailSenderImpl implements EmailSender {
 
-    public static final String MAIL_SMTP_HOST = "mail.smtp.host";
-    public static final String LOCALHOST = "localhost";
+    private static final String MAIL_SMTP_HOST = "mail.smtp.host";
+    private static final String MAIL_SMTP_PORT = "mail.smtp.port";
+    private static final String MAIL_SMTP_USERNAME = "mail.smtp.user.name";
+    private static final String MAIL_SMTP_PASSWORD = "mail.smtp.user.password";
+
+    private final Session session;
+
     private final String noReplyEmailAddress;
 
     public EmailSenderImpl(
-            @Value("${email.no-reply.address}") String noReplyEmailAddress) {
+            @Value("${email.no-reply.address}") String noReplyEmailAddress,
+            @Value("${" + MAIL_SMTP_HOST + "}") String mailSmtpHost,
+            @Value("${" + MAIL_SMTP_PORT + "}") int mailSmtpPort,
+            @Value("${" + MAIL_SMTP_USERNAME + "}") String mailSmtpUserName,
+            @Value("${" + MAIL_SMTP_PASSWORD + "}") String mailSmtpUserPassword) {
+
         this.noReplyEmailAddress = noReplyEmailAddress;
+
+        Properties config = new Properties();
+        config.put("mail.smtp.auth", "true");
+        config.put("mail.smtp.starttls.enable", "true");
+        config.put(MAIL_SMTP_HOST, mailSmtpHost);
+        config.put(MAIL_SMTP_PORT, mailSmtpPort);
+
+        this.session = Session.getInstance(config, new Authenticator() {
+
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(mailSmtpUserName, mailSmtpUserPassword);
+            }
+        });
     }
 
     @Override
@@ -42,7 +64,7 @@ public class EmailSenderImpl implements EmailSender {
                 emailData.getEmailContent().getImagesAbsolutePaths());
 
         try {
-            MimeMessage mimeMessage = prepareMimeMessage(emailData);
+            MimeMessage mimeMessage = prepareMimeMessage(emailData, session);
 
             Transport.send(mimeMessage);
         } catch (MessagingException e) {
@@ -50,11 +72,7 @@ public class EmailSenderImpl implements EmailSender {
         }
     }
 
-    private MimeMessage prepareMimeMessage(EmailData emailData) throws MessagingException {
-        Properties properties = System.getProperties();
-        properties.setProperty(MAIL_SMTP_HOST, LOCALHOST);
-        Session session = Session.getDefaultInstance(properties);
-
+    private MimeMessage prepareMimeMessage(EmailData emailData, Session session) throws MessagingException {
         MimeMessage mimeMessage = new MimeMessage(session);
         MimeMessageHelper message = new MimeMessageHelper(mimeMessage, true, StandardCharsets.UTF_8.name());
         message.setFrom(noReplyEmailAddress);
